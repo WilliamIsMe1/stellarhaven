81af14add6e5123e983a26c8bc3b05ddbebde838
William <unbelievable.half.insane@gmail.com>
2025-09-14 17:07:31 +0000

    Some cool commits :)

--- src/stellarhaven/view/GamePanel.java
+++ src/stellarhaven/view/GamePanel.java
@@ -15,9 +15,29 @@ public class GamePanel extends JPanel implements Runnable {
     public static final int SIZEX = 128;
     public static final int SIZEY = 128;
 
+    public static final int FPS = 60;
+
     @Override
     public void run() {
-        
+        while (true) {
+            long currentTime = System.currentTimeMillis();
+            long nextDrawTime = currentTime + 1000 / FPS;
+
+            update();
+            repaint();
+
+            if (System.currentTimeMillis() < nextDrawTime) {
+                try {
+                    Thread.sleep(nextDrawTime - System.currentTimeMillis());
+                } catch (InterruptedException e) {
+                    System.err.println("Man I hate runtime errors");
+                }
+            }
+        }
+    }
+
+    public void update() {
+        currentScene.update();
     }
 
     @Override
--- src/stellarhaven/view/scene/Scene.java
+++ src/stellarhaven/view/scene/Scene.java
@@ -1,8 +1,19 @@
 package stellarhaven.view.scene;
 
+import java.awt.Graphics2D;
+import java.util.HashMap;
+
 /**
- * A class representing a scene
+ * A class representing a scene, with methods to draw and update
  */
 public abstract class Scene {
+
+    public abstract void draw(Graphics2D g2);
+
+    public abstract void update();
+
+    public abstract void initialize(HashMap<String,String> properties);
+
+    public abstract void getProperty();
     
 }
