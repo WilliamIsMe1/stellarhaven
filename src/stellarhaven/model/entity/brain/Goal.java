e24f4fbc966d24938dc153abf71c4dcc02fd19a3
William <unbelievable.half.insane@gmail.com>
2025-09-29 20:01:21 +0000

    Changes

--- src/stellarhaven/model/entity/brain/Goal.java
+++ src/stellarhaven/model/entity/brain/Goal.java
@@ -1,9 +1,16 @@
 package stellarhaven.model.entity.brain;
 
+import stellarhaven.model.entity.Entity;
+
 /**
  * This class can be extended to MoveGoal, WorkGoal
  * 
  */
 public abstract class Goal {
-
+    /**
+     * 
+     * @param host This is the host that your life depends upon
+     * @return Whether or not {@link Goal} is met
+     */
+    public abstract boolean pursue(Entity host);
 }
