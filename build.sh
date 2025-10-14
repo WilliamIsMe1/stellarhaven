f07c013c6636eedf862aa4db991413eddd325a34
William <unbelievable.half.insane@gmail.com>
2025-10-14 16:25:38 +0000

    I'm making a build file and using terminal now :)

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+javac src/stellarhaven/*.java src/stellarhaven/model/*.java src/stellarhaven/model/entity/*.java src/stellarhaven/model/entity/brain/*.java src/stellarhaven/model/entity/genetics/*.java src/stellarhaven/model/structure/*.java src/stellarhaven/util/*.java src/stellarhaven/view/*.java src/stellarhaven/view/resources/*.java src/stellarhaven/view/resources/assets/*.java src/stellarhaven/view/scene/*.java -d bin/
